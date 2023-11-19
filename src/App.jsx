60f3da717812147318cbbd77d67d7ad4791fbc09
Klaudia Kieryk <klaudiakieryk123@gmail.com>
2023-11-19 23:04:24 +0100

    Prepare button component and use tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import Button from "./components/Button"
+
 const App = () => {
-    return <div>App</div>
+    return <div>
+        <div>
+            <Button success rounded outline>Click me</Button>
+        </div>
+        <div>
+            <Button danger outline>Buy Now!</Button>
+        </div>
+        <div>
+            <Button warning>See Deal!</Button>
+        </div>
+        <div>
+            <Button secondary outline>Something</Button>
+        </div>
+        <div>
+            <Button primary rounded>Hide Ads!</Button>
+        </div>
+    </div>
 }
 
 export default App
